0ce581b2a33d0231a5a099034e31184bff2dc212
igngkie <wysokinskikuba24@gmail.com>
2026-04-25 13:33:20 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "**50Kola👑**",
+        name: "50Kola👑",
         // Activity type number (0 = Playing).
         type: 2
       , 
